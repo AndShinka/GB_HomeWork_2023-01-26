e2b364b75d738140d8fe6e7111ac97212ab912c4
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-29 18:26:54 +0300

    Выяснить является ли число чётным

---
+++ 004_task_HomeWork/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Ведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2! == 0) Console.WriteLine("Число: " + num + " ЧЁТНОЕ");
+
+else Console.WriteLine("Число: " + num + " НЕ ЯВЛЯЕТСЯ ЧЁТНЫМ");
