dc8d1a238fce2437e80d8c4a60cb897f0a53b33e
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-29 17:27:07 +0300

    Найти максимальное из трех чисел

---
+++ 003_task_HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти максимальное из трех чисел
+
+int num1 = 99, num2 = 144, num3 = 56;
+int MaxOf3 = 0;
+
+if (num1 > num2) MaxOf3 = num1;
+else MaxOf3 = num1;
+
+if (num2 > MaxOf3) MaxOf3 = num2;
+
+if (num3 > MaxOf3) MaxOf3 = num3;
+
+Console.WriteLine("Найденное максимальное из трех чисел:" + MaxOf3);
