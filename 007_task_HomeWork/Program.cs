f6e409615b76c9f2365a34d5c0c7147790f5e3a0
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-29 20:21:02 +0300

    Показать вторую цифру трёхзначного числа

---
+++ 007_task_HomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int num = 0;
+
+do
+{
+   Console.WriteLine("Введите трёзначное число в диапазоне от 100 до 999: "); 
+   num = Convert.ToInt32(Console.ReadLine());
+}
+while ((num < 100) | (num >999));
+
+int lastDigit = (num / 10 %10);
+
+Console.WriteLine("В трехзначном числе " + num + ",  вторая цифра: " + lastDigit);
