283c1f6bdf937e5d0ef0d0c34b7724abb7527e6e
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-29 19:11:18 +0300

    Показать четные числа от 1 до N

---
+++ 005_task_HomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.WriteLine("Показываю чётные числа  от 1 до " + num + ": ");
+while (count <= num)
+{
+    if (count % 2 == 0) Console.Write(count + "  ");
+
+    count ++;
+}
+
+
