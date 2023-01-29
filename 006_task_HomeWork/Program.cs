20316ff3358d9d7f26aff6a124ec032b5cf90a12
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-29 19:57:59 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ 006_task_HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int num = 0;
+
+do
+{
+   Console.WriteLine("Введите трёзначное число в диапазоне от 100 до 999: "); 
+   num = Convert.ToInt32(Console.ReadLine());
+}
+while ((num < 100) | (num >999));
+
+int lastDigit = (num%10);
+Console.WriteLine("В трехзначном числе " + num + ", последняя цифра: " + lastDigit);
