f65bfbc6d441736ea03f584e9f5fe4ad22ffcec7
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-30 10:50:33 +0300

    кратно ли число заданному, если нет, вывести остаток.

---
+++ 010_task_HomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+//metka:
+int num1 = 0;
+int num2 = 0;
+do
+{
+   Console.WriteLine("Задайте первое число, на кратность которому будет проверяться второе число: "); 
+   num1 = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("Введите второе число (больше! первого), кратность которого первому числу проверяться: "); 
+   num2 = Convert.ToInt32(Console.ReadLine());
+}
+while (num1 > num2);
+
+if (num2 % num1 == 0) Console.WriteLine("Второе число " + num2 + " кратно первому " + num1);
+
+else Console.WriteLine("Второе число " + num2 + " НЕ кратно первому " + num1 + 
+                        ", остаток от деления равен: " + (num2 % num1));
+//goto metka;
