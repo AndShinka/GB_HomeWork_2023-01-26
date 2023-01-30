c108287171693db30144653a10163358f0634131
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-30 09:30:12 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 008_task_HomeWork/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int num = 0;
+
+do
+{
+   Console.WriteLine("Введите двуначное число в диапазоне от 10 до 99: "); 
+   num = Convert.ToInt32(Console.ReadLine());
+}
+while ((num < 10) | (num >99));
+
+int firstDigit = (num / 10 %10);
+int lastDigit = (num %10);
+
+Console.WriteLine("В двузначном числе " + num + ",  первая цифра: " + firstDigit+ ",  вторая цифра: " + lastDigit);
+
+if (firstDigit > lastDigit) Console.WriteLine("В двузначном числе " + num + ",  наибольшая цифра: " + firstDigit);
+else Console.WriteLine("В двузначном числе " + num + ",  наибольшая цифра: " + lastDigit);
+
+
