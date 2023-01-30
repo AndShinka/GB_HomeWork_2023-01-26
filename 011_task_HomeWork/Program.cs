78af585118caa6a12bef226db1749c534988c8df
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-01-30 11:28:34 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ 011_task_HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+//metka:
+
+int num = 0;
+Console.WriteLine("Введите число для эксперимента: "); 
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 100 > 0) Console.WriteLine("Третьей цифрой числа " + num + " является цифра: " + (num / 100 % 10));
+else Console.WriteLine("У числа " + num + " нет третьей цифры. ");
+
+Console.WriteLine();
+//goto metka;
